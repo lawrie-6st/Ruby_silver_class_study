99f3526ee484e66a487daad187675b7c7ae331d2
hirano-wataru <42793984+hirano-wataru@users.noreply.github.com>
2019-04-01 20:24:42 +0900

    chapter_3_to_p41

--- chapter_2/chapter_2.rb
+++ chapter_2/chapter_2.rb
@@ -1,1 +1,14 @@
 # ruby chapter_2/chapter_2.rb
+
+puts 'p18_組み込み定数とコマンドラインオプション'
+"
+STDIN = 標準入力
+STDOUT = 標準出力
+STDERR = 標準エラー出力
+ENV = 環境変数
+ARGF = 仮想ファイル
+ARGV = Rubyスクリプトに与えられた引数
+DATA = __END__以降をアクセスするfileオブジェクト
+"
+
+
